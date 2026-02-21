8512ab5f6d13fd4e631c73144bf48ffb5c54f209
Mahdi Bouaziz <bouazizmedmahdi@gmail.com>
2026-02-21 14:03:31 +0100

    main iam policy docuemnt

--- cloudfront-access/main.tf
+++ cloudfront-access/main.tf
@@ -1,9 +1,7 @@
 locals {
   bucket_arn = "arn:aws:s3:::${var.bucket_name}"
 
-  object_arns = length(var.prefixes) == 0
-    ? ["arn:aws:s3:::${var.bucket_name}/*"]
-    : [for p in var.prefixes : "arn:aws:s3:::${var.bucket_name}/${p}*"]
+  object_arns = length(var.prefixes) == 0 ? ["arn:aws:s3:::${var.bucket_name}/*"] : [for p in var.prefixes : "arn:aws:s3:::${var.bucket_name}/${p}*"]
 
   principal_role_arns = [
     for p in var.principals : "arn:aws:iam::${p.account_id}:role/${p.role_name}"
@@ -17,4 +15,31 @@ locals {
   list_bucket_actions  = var.allow_list ? ["s3:ListBucket"] : []
 
   existing_policy_json = var.existing_policy_json_path != null ? file(var.existing_policy_json_path) : null
+}
+
+data "aws_iam_policy_document" "cross_account_policy" {
+  dynamic "statement" {
+    for_each = local.has_role_principals && var.allow_list ? [1] : []
+    content {
+      sid    = "AllowCrossAccountListBucket"
+      effect = "Allow"
+
+      principals {
+        type        = "AWS"
+        identifiers = local.principal_role_arns
+      }
+
+      actions   = local.list_bucket_actions
+      resources = [local.bucket_arn]
+
+      dynamic "condition" {
+        for_each = length(var.prefixes) == 0 ? [] : [1]
+        content {
+          test     = "StringLike"
+          variable = "s3:prefix"
+          values   = [for p in var.prefixes : "${p}*"]
+        }
+      }
+    }
+  }
 }
